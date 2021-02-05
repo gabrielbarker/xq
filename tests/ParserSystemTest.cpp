d90e66bd027e06df5faf2ac6f3b2e03018ad01f5
Gabriel Barker <gabrielbarker@gmail.com>
2021-02-05 01:13:02 +0000

    ADD system test for parser

---
+++ tests/ParserSystemTest.cpp
@@ -0,0 +1,18 @@
+#include "Parser.hpp"
+#include "XML.hpp"
+#include "UnitTest++/UnitTest++.h"
+
+SUITE(ParserSystemTest)
+{
+  TEST(parseSimpleXML_parsesCorrectly)
+  {
+    FILE *f = fopen("tests/data/simple.xml", "r");
+    if (! f) {
+      std::cerr << "Open file failed" << std::endl;
+      exit(5);
+    }
+    Parser parser(f);
+    XML::Tag* root = parser.parse();
+    CHECK(root != nullptr);
+  }
+}
