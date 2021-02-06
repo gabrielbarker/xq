efdc9cb832ca2035ddf1ae11da8cd137ef61ef33
Gabriel Barker <gabrielbarker@gmail.com>
2021-02-06 01:01:14 +0000

    ADD more complex parser system tests

--- tests/ParserSystemTest.cpp
+++ tests/ParserSystemTest.cpp
@@ -1,18 +1,35 @@
 #include "Parser.hpp"
 #include "XML.hpp"
 #include "UnitTest++/UnitTest++.h"
+#include <string>
 
 SUITE(ParserSystemTest)
 {
-  TEST(parseSimpleXML_parsesCorrectly)
+  class ParserFixture
   {
-    FILE *f = fopen("tests/test_data/simple.xml", "r");
-    if (! f) {
-      std::cerr << "Open file failed" << std::endl;
-      exit(5);
+  public:
+    void parseFile(std::string path) 
+    {
+      FILE *f = fopen(path.c_str(), "r");
+      REQUIRE CHECK(f != nullptr);
+      Parser parser(f);
+      XML::Tag* root = parser.parse();
+      CHECK(root != nullptr);
     }
-    Parser parser(f);
-    XML::Tag* root = parser.parse();
-    CHECK(root != nullptr);
+  };
+  
+  TEST_FIXTURE(ParserFixture, parseSimpleXML_parsesCorrectly)
+  {
+    parseFile("tests/test_data/simple.xml");
+  }
+
+  TEST_FIXTURE(ParserFixture, parsePlantsXML_parsesCorrectly)
+  {
+    parseFile("tests/test_data/plants.xml");
+  }
+
+  TEST_FIXTURE(ParserFixture, parseSpecialXML_parsesCorrectly)
+  {
+    parseFile("tests/test_data/special.xml");
   }
 }
