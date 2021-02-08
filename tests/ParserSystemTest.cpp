464e9dbc41df46c1a1704f053753b91eedb7d577
Gabriel Barker <gabrielbarker@gmail.com>
2021-02-08 01:30:17 +0000

    ADD test checking parsed values

--- tests/ParserSystemTest.cpp
+++ tests/ParserSystemTest.cpp
@@ -9,19 +9,33 @@ SUITE(ParserSystemTest)
 {
   class ParserFixture
   {
-  public: XML::Tag* parseFile(std::string path) 
+  public: 
+    XML::Tag* parseFile(std::string path) 
     {
       FILE *f = fopen(path.c_str(), "r");
       REQUIRE CHECK(f != nullptr);
       Parser parser(f);
       XML::Tag* root = parser.parse();
-      CHECK(root != nullptr);
+      REQUIRE CHECK(root != nullptr);
       return root;
+    } 
+
+    void checkNameAndValueForTag(std::string name, std::string value, XML::Tag* tag) 
+    {
+      REQUIRE CHECK_EQUAL(name, tag->getName());
+      REQUIRE CHECK_EQUAL(value, tag->getValue());
     }
   };
   
   TEST_FIXTURE(ParserFixture, parseSimpleXML_returnsCorrectTagRoot)
   {
     XML::Tag* root = parseFile("tests/test_data/simple.xml");
+    REQUIRE CHECK_EQUAL(root->getName(), "meal");
+
+    std::vector<XML::Tag*> children = root->getChildren();
+    checkNameAndValueForTag("nationality", "french", children[0]);
+    checkNameAndValueForTag("type", "stew", children[1]);
+    checkNameAndValueForTag("vegetarian", "true", children[2]);
+    checkNameAndValueForTag("name", "VegetableStew", children[3]);
   }
 }
